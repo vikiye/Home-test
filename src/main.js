47d736957df2e83345f318ad326bbcf9a86359e2
vikiye <81535780+vikiye@users.noreply.github.com>
2021-05-18 16:22:21 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@ import App from './App.vue'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.prototype.$file; 
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
-
